7c5b3782afee9efc56192d35b01700776487cc4b
Damian Błaszczyk <60239406+damianblaszczyk@users.noreply.github.com>
2020-10-03 19:52:45 +0200

    Update core.pl

--- core.pl
+++ core.pl
@@ -23,7 +23,7 @@ sub Start
  		"localport=i"	=> \$configure->{LocalPort},
  		"remotehost=s"	=> \$configure->{RemoteHost},
  		"remoteport=i"	=> \$configure->{RemotePort},
- 		"ssl=i" 		=> \$configure->{SSL},
+ 		"ssl=i"			=> \$configure->{SSL},
  	),
 	or Usage();
 
